1d7c5413bff1e876b69b7dcf8d91aa82fb82ee82
martinkaburu <martinkaburu.m@gmail.com>
2018-08-31 07:31:23 +0300

    Update main.js

--- app/user_interface/gh-pages/static/main.js
+++ app/user_interface/gh-pages/static/main.js
@@ -1,3 +1,7 @@
 function show_hidden(id){
-      document.getElementById(id).style.display = "block";
+      if(document.getElementById(id).style.display == "block"){
+            document.getElementById(id).style.display = "none";
+      }else{
+      document.getElementById(id).style.display = "block"
+      }
   }
