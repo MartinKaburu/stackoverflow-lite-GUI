ab62f758a55354e95b3e1d9a485dba60b30d569c
Martin Kaburu <martinkaburu.m@gmail.com>
2018-09-02 18:53:39 +0300

    Fix main.js

--- app/user_interface/gh-pages/static/main.js
+++ app/user_interface/gh-pages/static/main.js
@@ -1,3 +1,8 @@
 function show_hidden(id){
-      document.getElementById(id).style.display = "block";
+      div = document.getElementById(id);
+      if(div.style.display == "block"){
+          div.style.display = "none";
+      }else{
+        div.style.display = "block";
+      };
   }
